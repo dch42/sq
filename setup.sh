344853b68aa79c26bf5cab2121a2fcf2d0342dfd
ddch42 <85430824+dch42@users.noreply.github.com>
2022-01-26 20:28:03 -0600

    add zsh check

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 declare personal_bin="/Users/$USER/bin"
+declare cfg=".bash_profile"
+
+[ -n $ZSH_VERSION ] && 
+cfg=".zprofile"
 
 [[ -d $personal_bin ]] || mkdir $personal_bin &&
-echo export PATH="$personal_bin:\$PATH" >> /Users/$USER/.bash_profile
+echo export PATH="$personal_bin:\$PATH" >> /Users/$USER/$cfg
 
 chmod +x ./sq.sh &&
 cp ./sq.sh $personal_bin/sq

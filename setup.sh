949d57c36639a2364a64be27d3ab5c311cff018a
ddch42 <85430824+dch42@users.noreply.github.com>
2022-05-08 22:27:33 -0700

    update, add linux

--- setup.sh
+++ setup.sh
@@ -1,13 +1,48 @@
 #!/bin/sh
 
-declare personal_bin="/Users/$USER/bin"
+declare script_name="sq"
 declare cfg=".bash_profile"
+
+declare grn="\e[0;92m"
+declare red="\e[0;91m"
+declare reset="\e[0m"
+
+function make_dir {
+    printf "Creating dir $personal_bin...\n" && 
+    mkdir $personal_bin
+    }
+
+function add_to_path {
+    printf "Adding '$personal_bin' to \$PATH in $cfg...\n" & 
+    echo export PATH="$personal_bin:\$PATH" >> /Users/$USER/$cfg
+}
+
+printf "\nAttempting installation of script '$script_name'...\n\n"
+
+echo "Installing requirements..."
+eval pip3 install -r requirements.txt
+
+case $OSTYPE in 
+    "darwin"*) declare personal_bin="/Users/$USER/bin" ;;
+    "linux-gnu"*) declare personal_bin="/home/$USER/bin" ;;
+    "linux"*) declare personal_bin="/home/$USER/bin" ;;
+esac
 
 [ -n $ZSH_VERSION ] && 
 cfg=".zprofile"
 
-[[ -d $personal_bin ]] || mkdir $personal_bin &&
-echo export PATH="$personal_bin:\$PATH" >> /Users/$USER/$cfg
+[[ ! -d $personal_bin ]] && 
+make_dir ||
+printf "'$personal_bin' exists...\n"
 
-chmod +x ./sq.sh &&
-cp ./sq.sh $personal_bin/sq
+grep -q "$personal_bin" /Users/$USER/$cfg && 
+printf "'$personal_bin' already in \$PATH...\n" ||
+add_to_path
+
+printf "\nInstalling $script_name...\n"
+chmod +x ./${script_name}.py &&
+cp ./${script_name}.py $personal_bin/$script_name &&
+printf "${grn}[SUCCESS]${reset} Script $script_name installed at '$personal_bin/$script_name'!\n\n" ||
+printf "${red}[ERROR]${reset} Something went wrong...\n" exit 1
+
+exit 0
